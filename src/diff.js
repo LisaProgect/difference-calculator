99599c23bfa57d8d53e8fa64b087f098fb443779
lisa <lisalis@vivaldi.net>
2022-06-24 19:44:23 +0300

    creat ast

--- src/diff.js
+++ src/diff.js
@@ -1,1 +1,65 @@
-export default (a, b, c) => console.log(a, b, c);
+import fs from 'fs';
+import _ from 'lodash';
+
+const getParseDataFile = (pathToFile) => {
+    const readFile = fs.readFileSync(pathToFile, 'utf8');
+    return JSON.parse(readFile);
+};
+
+const propertyActions = [
+    {
+        type: 'nested',
+        check: (valueBefore, valueAfter, key) =>
+            _.isObjectLike(valueBefore[key]) && _.isObjectLike(valueAfter[key]),
+        process: (valueBefore, valueAfter, fn) => ({
+            children: fn(valueBefore, valueAfter),
+        }),
+    },
+    {
+        type: 'add',
+        check: (valueBefore, valueAfter, key) =>
+            !_.has(valueBefore, key) && _.has(valueAfter, key),
+        process: (__, valueAfter) => ({ valueAfter }),
+    },
+    {
+        type: 'remove',
+        check: (__, valueAfter, key) => !_.has(valueAfter, key),
+        process: (valueBefore) => ({ valueBefore }),
+    },
+    {
+        type: 'change',
+        check: (valueBefore, valueAfter, key) =>
+            valueBefore[key] !== valueAfter[key],
+        process: (valueBefore, valueAfter) => ({ valueBefore, valueAfter }),
+    },
+    {
+        type: 'unchanged',
+        check: (valueBefore, valueAfter, key) =>
+            valueBefore[key] === valueAfter[key],
+        process: (valueBefore) => ({ valueBefore }),
+    },
+];
+
+const getPropertyAction = (dataBefore, dataAfter, key) =>
+    _.find(propertyActions, ({ check }) => check(dataBefore, dataAfter, key));
+
+const buildAst = (dataBefore, dataAfter) =>
+    _.union(_.keys(dataBefore), _.keys(dataAfter)).map((key) => {
+        const { type, process } = getPropertyAction(dataBefore, dataAfter, key);
+        return {
+            key,
+            type,
+            ...process(dataBefore[key], dataAfter[key], buildAst),
+        };
+    });
+
+const diff = (pathToFileBefore, pathFileToAfter, format) => {
+    const [contentBefore, contentAfter] = [
+        pathToFileBefore,
+        pathFileToAfter,
+    ].map((pathToFile) => getParseDataFile(pathToFile));
+    const ast = buildAst(contentBefore, contentAfter);
+    console.log(ast);
+};
+
+export default diff;
